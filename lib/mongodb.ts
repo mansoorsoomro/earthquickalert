61118212b6e5b580c54c5b6cd6312bfa2d688a7e
marianoor09 <marianoor09@outlook.com>
2026-02-23 20:06:24 +0500

    changes done on mongdb 1

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ready2go';
 
-if (MONGODB_URI.startsWith('mongodb+srv://') && !MONGODB_URI.includes('/', 14)) {
-    // If it's a sub-domain URI like cluster0.mongodb.net/ (with no DB name)
-    // Append the default DB name
-    MONGODB_URI = MONGODB_URI.endsWith('/') ? `${MONGODB_URI}ready2go` : `${MONGODB_URI}/ready2go`;
+if (MONGODB_URI.startsWith('mongodb+srv://')) {
+    const url = new URL(MONGODB_URI);
+    if (!url.pathname || url.pathname === '/') {
+        url.pathname = '/ready2go';
+        MONGODB_URI = url.toString();
+    }
 }
 
 /**
